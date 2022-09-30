e51355f54a525fa0d6b0e3e8e4bea72691921ebf
xuwuruoshui <1085252985@qq.com>
2022-09-30 21:52:20 +0800

    account_web注册心跳检查

--- account_web/main.go
+++ account_web/main.go
@@ -4,8 +4,22 @@ import (
 	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"goshop/account_web/handler"
+	"goshop/internal"
 )
+
+func init(){
+	randUUID := uuid.New().String()
+	err := internal.Reg(internal.AppConf.AccountWeb.Host,
+		internal.AppConf.AccountWeb.SrvName,
+		randUUID,
+		internal.AppConf.AccountWeb.Port,
+		internal.AppConf.AccountWeb.Tags)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main()  {
 	ip := flag.String("ip","192.168.0.112","输入IP")
